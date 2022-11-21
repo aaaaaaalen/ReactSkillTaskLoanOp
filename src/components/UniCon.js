1bad7b30e56cde7316b6d4b4e689fc3d5e484b73
aaaaaaalen <alenflynn@gmail.com>
2022-11-21 16:58:48 +1100

    adjust layout, add button
    function for each button

--- src/components/UniCon.js
+++ src/components/UniCon.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { Table, TableCell, TableRow, TableHead, TableBody, Button } from '@material-ui/core';
 import UniList from "../api/Uni.js";
 import { useEffect, useState } from "react";
-
+import { DataGrid} from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function UniCon() {
 
@@ -17,34 +19,42 @@ function UniCon() {
       setUni(response.data)
     }
 
+
+    const columns = [
+        { field: 'country', headerName: 'Country' , width: 100},
+        { field: 'domains', headerName: 'Domains', width: 200 },
+        { field: 'web_pages', headerName: 'Web Pages', width: 200},
+        { field: 'alpha_two_code', headerName: 'Country Code', width: 150 },
+        { field: 'name', headerName: 'University Name', width: 300 },
+        { field: 'state-province', headerName: 'State', width: 100 }
+      ]
+
+
     return (
-        <Table>
-          <TableHead>
-              <TableRow>
-                  <TableCell>Country</TableCell>
-                  <TableCell>Domains</TableCell>
-                  <TableCell>Web page</TableCell>
-                  <TableCell>Country Code</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>State</TableCell>
-              </TableRow>
-          </TableHead>
-          <TableBody>
-          {
-            uni.map((data) => (
-                  <TableRow>
-                      <TableCell>{data.country}</TableCell>
-                      <TableCell>{data.domains}</TableCell>
-                      <TableCell>{data.web_pages}</TableCell>
-                      <TableCell>{data.alpha_two_code}</TableCell>
-                      <TableCell>{data.name}</TableCell>
-                      
-                      <TableCell>{data.state-province}</TableCell>
-                  </TableRow>
-              ))
-          }
-          </TableBody>
-        </Table>
+      <Box sx={{ height: '100%', width: '100%' }}>
+        <Stack direction="row" spacing={1} sx={{ mb: 1 }}>
+          <Button size="small">
+            Load table
+          </Button>
+          <Button size="small">
+            Remove a row
+          </Button>
+          <Button size="small">
+            Add a row
+          </Button>
+        </Stack>
+        <div style={{ height: 800, width: 1200 }}>
+          <DataGrid     
+              
+              getRowId={uni => uni.name}
+              rows={uni}
+              columns={columns}
+              pageSize={15} rowsPerPageOptions={[15, 30, 50]}
+            />
+        </div>
+        
+      </Box>
+
     );
 }
 export default UniCon
--- src/pages/UniList.js
+++ src/pages/UniList.js
@@ -1,41 +1,28 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import TopBar from "../components/TopBar";
 import ContactsContainer from "../components/UniCon";
 import { makeStyles } from '@mui/styles';
 import './UniList.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex"
-  },
-
-  top:{
-    position:'fixed',
-    top:0,
-    width:'100%',
-    zIndex: 9999,
+    display: "flex",
   },
   content:{
-    padding: '10px',
-    margin: '100px',
+    padding: '100px',
+    margin: 'auto',
   }
 }));
 
 export default function App() {
   const classes = useStyles();
   return (
-    <div className="App">
       <Router>
-          <main className={classes.root}>
-            <div className={classes.top}>
-                <TopBar />
-            </div>        
+          <main className={classes.root}>       
             <div className={classes.content}>
                 <ContactsContainer />
             </div>               
         
           </main>      
       </Router>
-    </div>
   );
 }
